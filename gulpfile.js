d24e77f3707efc3d920807cc3063798608586b56
Alexey Kulakov <uni_que@me.com>
2017-06-18 21:44:42 -0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/style/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', function () {
+  gulp.watch('./src/style/**/*.scss', ['sass']);
+});
